e615b41efa37e796f7ac2e8bdf4164f1f8985ac7
swon95 <shimtkddnjs13@gmail.com>
2023-11-02 20:04:04 +0900

    typescript day2

---
+++ Typescript/workspace/ch02/ex02-13.ts
@@ -0,0 +1,11 @@
+(()=>{
+    function echo<T extends string | number>(msg: T): T{
+      return msg;
+    }
+  
+    console.log(echo('hello'));
+    console.log(echo(100));
+    console.log(echo(new Date()));
+  })();
+  
+  
