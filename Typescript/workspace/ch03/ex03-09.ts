18804d1bf9318a870947325c6b0ac7a7c821a182
swon95 <shimtkddnjs13@gmail.com>
2023-11-04 02:03:14 +0900

    typescript day3-2

--- Typescript/workspace/ch03/ex03-09.ts
+++ Typescript/workspace/ch03/ex03-09.ts
@@ -1,3 +1,19 @@
 (()=>{
-  
+    // 기본 데이터 타입은 추론에 맞기고
+  var name = 'kim'
+  var age = '30'
+  var address: string // 선언을 먼저 할 경우 타입을 지정하기
+  address = '경기도'
+//   address = 100
+
+interface Todo {
+    title: string
+    content: string
+}
+
+// 객체는 타입 별칭이나 인터페이스를 지정해야 안전하게 사용 가능
+var todo1: Todo = {
+    title: '제목 1',
+    content: '내용 1',
+}
 })();
--- Typescript/workspace/ch03/ex03-19.ts
+++ Typescript/workspace/ch03/ex03-19.ts
@@ -1,4 +1,33 @@
 // ex03-18.ts 복사
 (()=>{
+    const kim: User = {
+      name: '김철수',
+      age: 30,
+      nickname: '철수'
+    };
   
-})();
+    const lee: UnknownUser = {
+      name: '이영희',
+      age: '비공개'
+    };
+  
+    const hong: AdminUser = {
+      name: '홍길동'
+    };
+  
+    function printUser(user: User | UnknownUser | AdminUser){
+      // User 타입일 경우 나이 출력
+      if(isUser(user)){
+        console.log(user.nickname);
+      }
+    }
+  
+    // 타입 가드 함수
+    function isUser(user: User | UnknownUser | AdminUser): user is User{
+      return typeof (user as User).age === 'number';
+    }
+  
+    printUser(kim);
+    printUser(lee);
+    printUser(hong);
+  })();
