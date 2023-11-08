73d02846cf2b6ae4cd82c045096041530a43e6e7
swon95 <shimtkddnjs13@gmail.com>
2023-11-09 01:33:23 +0900

    useEffect 내부에서 State 와 Ref 의 차이

--- React/Hooks/src/App.js
+++ React/Hooks/src/App.js
@@ -1,39 +1,48 @@
-import React, { useState, useRef } from "react";
+// ex) useEffcet case
+
+// import React, { useState, useEffect } from "react";
+// import "./App.css";
+
+// function App(): Element {
+//     const [count, setCount] = useState(0);
+//     const [renderCount, setRenderCount] = useState(1);
+
+//     // 무한 루프
+//     useEffect((): void => {
+//         console.log("렌더링! ");
+//         // useEffcet 훅 내부에서, State 를 강제로 변경하면 App 함수는 재 렌더링됨
+//         setRenderCount(renderCount + 1);
+//     });
+//     return (
+//         <div>
+//             <p>Count: {count}</p>
+//             <button onClick={(): void => setCount(count + 1)}>증가</button>
+//         </div>
+//     );
+// }
+
+// export default App;
+
+// ----------------------------------------------------------------------------------------
+// ex) useRef case
+
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App(): Element {
-    // State 는 값도 변하고, 렌더링도 시킴
     const [count, setCount] = useState(0);
-    // Ref 는 값은 변하지만, 렌더링은 시키지 않음
-    const countRef = useRef(0);
-    // Var 는 일반 변수의 경우 컴포넌트 생애주기와 연관이 있어 초기화 될 수 있지만,
-    // Ref 와 유사하게 값은 변하지만 렌더링은 되지 않음
-    let countVar = 0;
-    console.log("렌더링");
+    const renderCount = useRef(1);
 
-    const increaseCountState = (): void => {
-        setCount(count + 1);
-    };
-    const increaseCountRef = (): void => {
-        countRef.current = countRef.current + 1;
-        console.log("Ref: ", countRef.current);
-    };
-    const increaseCountVar = (): void => {
-        countVar = countVar + 1;
-        console.log("var: ", countVar);
-    };
-    const printResult = (): void => {
-        console.log(`ref: ${countRef.current}, var: ${countVar}`);
-    };
+    // 무한 루프
+    useEffect((): void => {
+        console.log("렌더링! ", renderCount.current);
+        // Ref 는 렌더링에 영향이 없기 때문에, 값은 유지하되 State 는 변경하고 재 랜더링 x
+        renderCount.current = renderCount.current + 1;
+    });
     return (
         <div>
-            <p>State: {count}</p>
-            <p>Ref: {countRef.current}</p>
-            <p>Var: {countVar}</p>
-            <button onClick={increaseCountState}>상태 +1(렌더링)</button>
-            <button onClick={increaseCountRef}>Ref +1</button>
-            <button onClick={increaseCountVar}>변수 +1</button>
-            <button onClick={printResult}>결과 보기</button>
+            <p>Count: {count}</p>
+            <button onClick={(): void => setCount(count + 1)}>증가</button>
         </div>
     );
 }
